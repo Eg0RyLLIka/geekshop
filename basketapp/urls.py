6714661805f0eaee8a36f61009aa27810e8db0be
Egor <byk-2010@mail.ru>
2021-11-27 13:21:29 +0300

    Lesson 9. Code style fix

--- basketapp/urls.py
+++ basketapp/urls.py
@@ -11,4 +11,3 @@ urlpatterns = [
     re_path(r"^remove/(?P<pk>\d+)/$", basketapp.basket_remove, name="remove"),
     re_path(r"^edit/(?P<pk>\d+)/(?P<quantity>\d+)/$", basketapp.basket_edit, name="edit"),
 ]
-
